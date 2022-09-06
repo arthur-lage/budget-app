2f23146b47d810fbebd8282fde4ab6d88ff53c9e
Arthur Lage <arthurlage2006@gmail.com>
2022-09-05 22:24:25 -0300

    landing page ready and responsive

--- web/src/pages/LandingPage/index.tsx
+++ web/src/pages/LandingPage/index.tsx
@@ -1,24 +1,43 @@
 import { Link } from "react-router-dom";
 
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
-import Illustration from '../../assets/landing-page-illustration.svg'
+import Illustration from "../../assets/landing-page-illustration.svg";
+import { useState } from "react";
 
 export function LandingPage() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className={styles.container}>
       <header>
-        <Link className={styles.logo} to="/">Budget</Link>
+        <Link className={styles.logo} to="/">
+          Budget
+        </Link>
+        <div
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className={`${styles.hamburger} ${isMenuOpen ? styles.open : ""}`}
+        >
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </div>
         <nav>
           <ul>
             <li>
-              <Link className={styles.about} to="/about">About</Link>
+              <Link className={styles.about} to="/about">
+                About
+              </Link>
             </li>
             <li>
-              <Link className={styles.login} to="/login">Login</Link>
+              <Link className={styles.login} to="/login">
+                Login
+              </Link>
             </li>
             <li>
-              <Link className={styles.register} to="/register">Get Started</Link>
+              <Link className={styles.register} to="/register">
+                Get Started
+              </Link>
             </li>
           </ul>
         </nav>
@@ -26,9 +45,17 @@ export function LandingPage() {
 
       <main>
         <h1 className={styles.title}>Budget</h1>
-        <p className={styles.description}>Manage your personal finances easily.</p>
-        <img src={Illustration} alt="Man looking at a graph on a computer screen" />
-        <button className={styles.getStarted}>Get Started</button>
+        <p className={styles.description}>
+          Manage your personal finances easily.
+        </p>
+        <img
+          className={styles.image}
+          src={Illustration}
+          alt="Man looking at a graph on a computer screen"
+        />
+        <Link to="/register" className={styles.getStarted}>
+          Get Started
+        </Link>
       </main>
     </div>
   );
